8c9da4aa17fa1056f07435455ec3d73869f7e78d
Genaro Coronel <genaro.coronel561@gmail.com>
2024-02-25 01:25:19 -0600

    Rertored the original dependencies

--- replit.nix
+++ replit.nix
@@ -1,7 +1,23 @@
 {pkgs}: {
   deps = [
+    pkgs.rustc
+    pkgs.libiconv
+    pkgs.cargo
+    pkgs.python310
+    pkgs.python310Packages.pydantic
+    pkgs.python310Packages.zeep
+    pkgs.python310Packages.xmlsec
+    pkgs.python310Packages.dicttoxml
+    pkgs.python310Packages.attrs
+    pkgs.libtool
     pkgs.libxcrypt
     pkgs.zlib
     pkgs.xcodebuild
+    pkgs.cacert
+    pkgs.openssl
+    pkgs.pkg-config
+    pkgs.coreutils
+    pkgs.libxml2
+    pkgs.streamlit
   ];
 }

ff1dd461cb297e2b01d5de467805347184c3e28a
Genaro Coronel <genaro.coronel561@gmail.com>
2024-02-26 21:58:15 -0600

    Updated dependencies

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import streamlit as st
-from replit import db
+import xmlsec
+try:
+  from replit import db
+except ModuleNotFoundError:
+  db = {}
 
 st.title("Streamlit on Replit")
 
@@ -7,6 +11,7 @@ text = f"""
 
 - More information on streamlit [here](https://docs.streamlit.io/)
 - db: {db.keys()}
+- xk: {xmlsec.Key}
 
 """
 

3bde92fc430ec83cfcc68f6c0942a948835ece80
Genaro Coronel <74608289+genarocoronel@users.noreply.github.com>
2024-03-08 07:57:44 +0000

    The key, password and request are saved to the database

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-import caller
+import caller, base64
 import streamlit as st
 from replit import db
 if db is None:
@@ -10,6 +10,15 @@ def do_API_call(source, key_file, pswd):
     except Exception as e:
         return (str(e), source)
 
+def decode_source(source):
+    toreturn = [None] * len(source)
+    for ibytes in (0, 1):
+        if isinstance(source[ibytes], str):
+            toreturn[ibytes] = base64.b64decode(source[ibytes].encode("utf-8"))
+            if ibytes == 0:
+                toreturn[ibytes] = toreturn[ibytes].decode("utf-8")
+            #assert 0, (toreturn[ibytes], ibytes, source)
+    return toreturn
 #with st.form("Authentication"):
 key_file = st.file_uploader("key", type=None, accept_multiple_files=False, key=None, help=None, on_change=None, args=None, kwargs=None,
              disabled=False, label_visibility="visible")
@@ -27,21 +36,33 @@ button = st.button('Test API call')
 
 if button:
     if xmlta:
-        source = xmlta
+        #source = xmlta
+        source = base64.b64encode(xmlta.encode("utf-8")).decode('utf-8')
     else:
-        source = xmlfp.read()
+        if xmlfp is None:
+            st.error("No XML file or text provided")
+            source = None
+        else:
+            source = base64.b64encode(xmlfp.read()).decode('utf-8')
+            #source = xmlfp.read().decode('utf-8')
     if key_file is None:
-        key = None
+        key = ""
     else:
-        key = key_file.read()
-    #assert 0, (pswd,)
-    to_save = (source, key, pswd)
-    #call = do_API_call(source, key_file, pswd)
-    #to_save = (source, key, pswd, call)
-    st.write("Saving data", to_save)
-    db_index = f"call-{len(db)+1}"
-    db[db_index] = to_save
-    print("Saving data", to_save)
-    st.write("Number of requests in the database:", len(db))
-    st.write("Retrieving data", db[db_index])
+        key = base64.b64encode(key_file.read()).decode('utf-8')
+        #key = key_file.read().decode('utf-8')
+    if source is not None:
+        to_save = [source, key, pswd]
+        #call = do_API_call(source, key_file, pswd)
+        #to_save = (source, key, pswd, call)
+        #st.write("Saving data", to_save)
+        #print("Saving data", to_save)
+        db_index = f"call-{len(db)+1}"
+        db[db_index] = to_save
+        #st.write("Number of requests in the database:", len(db))
+        new_source = db[db_index]
+        new_source_decoded = decode_source(new_source)
+        #st.write("Retrieving datasrc", new_source)
+        #st.write("Retrieving data", new_source_decoded)
+        print("new_source_decoded:", new_source_decoded)
+        st.write("Request saved to the database")
 st.write()
